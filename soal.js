a4db8f4e291ab2284e9a084eee2a9b25789328cf
unknown <mustafid.mk@gmail.com>
2023-01-05 23:58:14 +0700

    hosting 2

---
+++ soal.js
@@ -0,0 +1,79 @@
+let MCQS = [{
+    question: "Apa Kepanjangan Dari HTML?",
+    choice1: "Hyperlinks and Text Markup Language",
+    choice2: "Hyper Text Markup Language",
+    choice3: "Hyper Text Making Language",
+    choice4: "Hyper Text Mark Language",
+    answer: 1
+},
+            {
+            question: "Apa Kepanjangan Dari CSS?",
+    choice1: "Colorful StyleSheet",
+    choice2: "Creative Style Sheet",
+    choice3: "Cascading Style Sheet",
+    choice4: "Computer Style Sheet",
+    answer: 2
+},
+            {
+            question: "Tag HTML mana yang digunakan untuk mendefinisikan internal css?",
+    choice1: "<script>",
+    choice2: "<style>",
+    choice3: "<html>",
+    choice4: "<svg>",
+    answer: 1
+},
+            {
+            question: "Syntax mana yang benar untuk styling css?",
+    choice1: "body{color:black}",
+    choice2: "{body{color:black}",
+    choice3: "body={color:black}",
+    choice4: "body:color{black}",
+    answer: 0
+},
+            {
+            question: "Bagaimana Cara Memberi Comment Di CSS?",
+    choice1: "/*Ini Comment*/",
+    choice2: "//Ini Comment",
+    choice3: "<!--- Ini Comment --->",
+    choice4: "//Ini Comment//",
+    answer: 1
+},
+            {
+            question: "Bagaimana Cara Untuk Memberi Comment Di HTML",
+    choice1: "/*Ini Comment*/",
+    choice2: "//Ini Comment",
+    choice3: "<!--- Ini Comment --->",
+    choice4: "//Ini Comment//",
+    answer: 2
+},
+            {
+               question: "Properti mana digunakan untuk mengubah warna latar belakang?",
+    choice1: "backgroundColor",
+    choice2: "BgColor",
+    choice3: "Color-Background",
+    choice4: "background",
+    answer: 3
+},{
+               question: "Bagaimana cara menulis IF statement dalam JavaScript?",
+    choice1: "if i==5",
+    choice2: "if(i==5)",
+    choice3: "if(i==5)then",
+    choice4: "if i==5 then",
+    answer: 2
+},
+            {
+               question: "Di dalam elemen HTML mana kita meletakkan JavaScript?",
+    choice1: "<js></js>",
+    choice2: "<javascript></javascript>",
+    choice3: "<script></script>",
+    choice4: "<scripting>",
+    answer: 2
+},
+            {
+               question: "Bagaimana loop WHILE dimulai?",
+    choice1: "while(i <= 0)",
+    choice2: "while(i <= 0 i++)",
+    choice3: "while i <= 0",
+    choice4: "while (i++ i <= 0)",
+    answer: 0
+}];
